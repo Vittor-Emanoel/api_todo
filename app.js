02f5561f0e9e2de72d820d2bcac8aea9813d35e5
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-01-30 17:04:18 -0300

    :tada: create route get users in express

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello, welcome to my api server')
 })
 
+app.get('/users', (req, res) => {
+  res.status(200).json()
+})
+
 app.listen(3000, () => {
   console.log('listening on port 3333')
 })
