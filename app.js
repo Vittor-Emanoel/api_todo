617ee1ef5ac90b387f4b7015d364d4af2d6b0f0e
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-01-30 17:08:21 -0300

    :tada: testing route get users

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ import express from 'express'
 
 const app = express()
 
+const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    age: 20,
+  },
+  {
+    id: 2,
+    name: 'Waller',
+    age: 30,
+  },
+]
+
 app.get('/', (req, res) => {
   res.send('Hello, welcome to my api server')
 })
 
 app.get('/users', (req, res) => {
-  res.status(200).json()
+  res.status(200).json(users)
 })
 
 app.listen(3000, () => {
