87a90b6f4f1da5b6c95ba1fc571558387ab3feb1
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-01 14:11:00 -0300

    add vercel config.josn

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 
 mongoose.connect(
   'mongodb+srv://vittor:300321Vb@cluster0.izrwffs.mongodb.net/?retryWrites=true&w=majority'
 )
+
+console.log(dotenv)
 
 let db = mongoose.connection
 
