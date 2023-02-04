4df76530cf801c40446a588e7765952e1aae9df6
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-03 21:25:27 -0300

    add new method cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-Requested-With,content-type, Authorization'
